c46a85d88524e172f5306cacbdca92f956fdd670
maxflex <>
2019-03-29 16:25:36 +0300

    JSON only on api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\JsonMiddleware::class,
     ];
 
     /**
@@ -39,6 +38,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \App\Http\Middleware\JsonMiddleware::class,
             'throttle:60,1',
             'bindings',
         ],
